55b0e4b7e2fa786ed47857c6c06612b5d0f1f830
Anagha-02 <79145408+Anagha-02@users.noreply.github.com>
2021-09-24 17:40:48 +0530

    button functionality using jss

--- jsxexample/src/App.js
+++ jsxexample/src/App.js
@@ -1,26 +1,87 @@
-import { useState } from 'react';
+import React, { useState } from "react";
+import { createUseStyles } from 'react-jss'
 
-const bgred = {
-  background: 'red',
-  align: 'center'
-}
-const bgblue = {
-  background: 'blue',
-  align: 'center'
-}
 
-function App() {
+const useStyles = createUseStyles({
+  divStyle: {
+    alignItems: 'center',
+    margin:50
+  },
+  bgRed: {
+    background: 'red',
+    align: 'center'
+  },
+  bgBlue: {
+    background: 'blue',
+    align: 'center'
+  }
+})
+
+function Button({ children, ...props }) {
   const [toggle, setToggle] = useState(true);
+  const classes = useStyles({ ...props })
 
-  let btn_class = toggle ? bgred : bgblue;
+  const onClickHandler = () => {
+    setToggle(!toggle);
+  }
 
   return (
-    <div style={{margin:10}}>
-      <button style={btn_class} onClick={() => { setToggle(!toggle); }}>
-        Button
+    <div className={classes.divStyle}>
+      <button className={toggle ? classes.bgRed : classes.bgBlue} onClick={onClickHandler}>
+        <span>{children}</span>
       </button>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () =>
+  <Button> Submit </Button>
+
+export { App };
+
+/*
+let toggle = true;
+
+const useStyles = createUseStyles((theme) => (
+  {
+    myButton: {
+      toggle: theme.toggleNew
+    },
+    myLabel: (props) => (toggle ? {
+      background: 'red',
+      align: 'center'
+    } :
+      {
+        background: 'blue',
+        align: 'center'
+      }
+    )
+  }
+))
+
+const theme = {
+  toggleNew: true,
+  colorSecondary: 'blue'
+}
+
+const onClickHandler = () => {
+  toggle = !toggle;
+  console.log(toggle);
+}
+
+const Button = ({ children, ...props }) => {
+  const theme = useTheme()
+  const classes = useStyles({...props, theme})
+  return (
+    <button className={toggle ? classes.bgRed : classes.bgBlue} onClick={onClickHandler}>
+      <span>{children}</span>
+    </button>
+  )
+}
+
+
+const App = () =>
+  <Button>Submit</Button>
+
+export { App };
+*/
